0d20da058f06ebaabdc4afd7ec9c5909a1223d91
Destry Ullrich <wullrich@cnm.edu>
2024-07-01 09:20:33 -0600

    Completed the variables and data video, logged all examples

---
+++ variables-and-data.js
@@ -0,0 +1,74 @@
+let stringData;
+stringData = "Data made of letters";
+/* In case anyone reads this, I don't know how to let
+    webstorm allow me to declare and assign on the same line*/
+console.log(stringData);
+
+///////////////////////////////////
+
+let total;
+let tax;
+let afterTax;
+
+total=10.57
+tax=1.07
+afterTax=total*tax
+
+///////////////////////////////////
+
+let boolean;
+let number;
+let string;
+
+let object;
+
+boolean = false;
+number = 4.5673
+string = "Words, words, words."
+
+object = {
+    item1: "string data",
+    item2: 3.14159,
+    item3: true,
+    item4: {
+        subItem: "objectInception!"
+    }
+}
+
+console.log(object.item4.subItem)
+
+///////////////////////////////////
+
+let var1;
+let var2;
+
+var1=5
+var2="2"
+
+let result;
+result=var1+var2
+console.log(result)
+
+////////////////////////////////////
+
+let add;
+let sub;
+let mult;
+let divi;
+let mod;
+
+add= 1+1;
+sub= 1-1;
+multi= 1*2;
+divi= 2/1;
+mod= 3%2;
+
+let stringA;
+let stringB;
+let conc;
+
+stringA= "Hello ";
+stringB= "World";
+conc= stringA+stringB;
+
+console.log(conc);
